30d7dcca415005a3c1533e7fc6a97033a608f92f
rajata23 <rajatagrawal785@gmail.com>
2020-07-28 13:49:23 +0530

    revert back to 8fab4ae38abbb1ae43f80762872551187b87e14a

--- app.js
+++ app.js
@@ -1,5 +1,1 @@
 console.log("Complex app!!!!");
-console.log("Another LIne");
-console.log("Another LIne Very different from erlier one");
-console.log("Complex app! Very different from erlier one!!!");
-sbchbbdvkhj cugbdh g ugBHLUEBEKJKJKJ
